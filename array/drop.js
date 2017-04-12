d7c244471c60e69e37121650b0614da44a968599
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-04-12 15:46:05 +0800

    added drop.js two functions

---
+++ array/drop.js
@@ -0,0 +1,13 @@
+function _drop(array, num = 1) {
+  return array.slice(num, array.length);
+}
+
+function _drop(array, num = 1) {
+  var res = [];
+  for (let i = num; i < array.length; i++) {
+    res.push(array[i]);
+  }
+  return res;
+}
+
+console.log(_drop([1 ,2 , 3, 4], 5));
